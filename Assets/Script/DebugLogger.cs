363be2489dbd7bacb0dccc8111aec863fc560b1e
kimjoy2002 <kimjoy2002@naver.com>
2019-07-23 00:14:43 +0900

    디버깅로거 실제 빌드에서 제외

--- Assets/Script/DebugLogger.cs
+++ Assets/Script/DebugLogger.cs
@@ -6,10 +6,12 @@ public class DebugLogger
     /// <summary>
     /// Debug.Log를 랩핑하였음. FIXME 나중에 debug일때 행동을 피하도록 해봐야지
     /// </summary>
+    [System.Diagnostics.Conditional("ENABLE_LOG")]
     public static void Log(object message)
     {
         Debug.Log(message);
     }
+    [System.Diagnostics.Conditional("ENABLE_LOG")]
     public static void Log(string format, params object[] args)
     {
         Debug.Log(string.Format(format, args));
