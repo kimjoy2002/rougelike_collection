82583c8bd2fd4b0efa5874825f49afa4af09af95
kimjoy2002 <kimjoy2002@naver.com>
2019-11-14 20:03:55 +0900

    빌드 스크립트 추가

---
+++ Assets/Script/WebGLBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class WebGLBuilder
+{
+    static void build()
+    {
+        string[] scenes = { "Assets/Scenes/MainGame.unity" };
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, "WebGL-Dist", UnityEditor.BuildTarget.WebGL, UnityEditor.BuildOptions.None);
+    }
+}
